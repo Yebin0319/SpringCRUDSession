da37d0505debb8a4abfe987e422139dc8ef5b9cf
dohee77 <dodo040906@gmail.com>
2026-04-09 18:46:15 +0900

    Update BoardCreateRequest.java

--- src/main/java/com/likelion/session/dto/BoardCreateRequest.java
+++ src/main/java/com/likelion/session/dto/BoardCreateRequest.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter // 모든 필드의 getter 메서드를 자동 생성
+@Setter // 모든 필드의 setter 메서드를 자동 생성
+@NoArgsConstructor // 기본 생성자(파라미터 없는 생성자) 생성
+@AllArgsConstructor // 모든 필드를 포함한 생성자 생성
 public class BoardCreateRequest {
     // 넘겨주고 싶은 정보: 제목(title), 내용(content), 작성자(writer)
     private String ;
